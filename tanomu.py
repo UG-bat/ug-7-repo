500430f52ab8531f06b083e3bf5874c76349e5f9
小野田裕史 <ondyuji@gmail.com>
2024-10-11 14:13:29 +0900

    Change all of the code

--- tanomu.py
+++ tanomu.py
@@ -1,43 +1,27 @@
 import streamlit as st
-from onegai.onegai import show as onegai_show  # onegaiディレクトリからモジュールをインポート
-from onegai.kitekure import show as kitekure_show
 
-# ページ初期設定
+
 def init_page():
     st.set_page_config(
-        page_title="Ask about PDF(s)",
-        page_icon="📄"
+        page_title="Ask My PDF(s)",
+        page_icon="🧐"
     )
+
 
 def main():
     init_page()
-    
-    st.sidebar.title("Navigation")  # サイドバーにタイトルを設定
-    page = st.sidebar.selectbox("Select a page", ["Home", "Upload PDF", "PDF QA"])  # ページ選択
 
-    # 各ページの表示
-    if page == "Home":
-        st.title("Home Page")
-        st.write("This is the home page.")
-    elif page == "Upload PDF":
-        onegai_show()  # onegai.pyの中のshow関数を呼び出す
-    elif page == "PDF QA":
-        kitekure_show()  # kitekure.pyの中のshow関数を呼び出す
-
-    st.sidebar.success("上のメニューから選択してください。")
+    st.sidebar.success("👆のメニューから進んでね")
 
     st.markdown(
     """
-    ### Ask about PDF(s) 
+    ### Ask My PDF(s) にようこそ！
 
-    - このアプリでは、アップロードしたPDFについての質問をすることができます。
-    - まずは左のメニューから `upload_pdf` を選択してPDFをアップロードしてください。
-    - PDFをアップロードしたら `pdf_QA` を選択して質問をしてみましょう。
+    - このアプリでは、アップロードしたPDFに対して質問をすることができます。
+    - まずは左のメニューから `📄 Upload PDF(s)` を選択してPDFをアップロードしてください。
+    - PDFをアップロードしたら `🧐 PDF QA` を選択して質問をしてみましょう😇
     """
     )
 
-
-# メイン関数の実行
 if __name__ == '__main__':
     main()
-
